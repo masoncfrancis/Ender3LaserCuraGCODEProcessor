a501d76569f8b87e2a6ab4c2b3ae160bff178238
coolmnoisy <coolmnoisy@yahoo.com>
2022-10-15 00:26:18 -0600

    Wrote a basic script in e3proc.py that turns the laser on and off at appropriate intervals. The rest of the changes need to be made manually.

--- src/e3proc.py
+++ src/e3proc.py
@@ -9,15 +9,25 @@ if __name__ == '__main__':
     destinationPath = input("Please enter destination file path (any existing file will be overwritten): ")
     sourceFile = open(sys.argv[1])
     destFile = open(destinationPath, "w")
-    previousLine = ["G0"]
+    previousLine = ['G0']
     for line in sourceFile:
         splitLine = line.split()
-        if splitLine[0] == "G1" and previousLine[0] == "GO":
-            destFile.write("M106")
+        try:
+            test = splitLine[0]
+        except:
+            break
+        splitG1 = splitLine[0] == 'G1'
+        splitG0 = splitLine[0] == 'G0'
+        prevG1 = previousLine[0] == 'G1'
+        prevG0 = previousLine[0] == 'G0'
+        if splitG1 and prevG0:
+            destFile.write("M106\n")
             previousLine = splitLine
-        elif splitLine[0] == "G0" and previousLine[0] == "G1":
-            destFile.write("M107")
+        elif splitLine[0] == 'G0' and previousLine[0] == 'G1':
+            destFile.write("M107\n")
             previousLine = splitLine
         destFile.write(line)
+
+    print("File processed successfully. Exiting...")
     
             
